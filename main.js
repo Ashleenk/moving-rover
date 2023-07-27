3838beafc2081a313122c2147928657a8c8b6a97
Ashleen <117347894+Ashleenk@users.noreply.github.com>
2023-07-27 19:07:58 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function uploadBackground() {
+ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height;)    
+}    
+function uploadrover() {
+ctx.drawImage(rover_imgTag, 0, 0, rover_x, rover_y rover.width, rover.height;)    
+}    
+window.addEventListener("keydown",my_keydown)
+function my_keydown(e)
+{
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed=='38')
+{
+    
+    up();
+    console.log("up")
+}
+{
+    if(keyPressed=='40')
+}
+{
+        down();
+        console.log("down")
+}
+ {
+         if(keyPressed=='37')
+}
+ {
+      left();
+        console.log("left")
+ }
+{
+     if(keyPressed=='39')
+}
+{
+  right();
+        console.log("right")
+ }
+}
